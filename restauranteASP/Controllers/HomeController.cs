49c5283414b4abe313f876fa231cf2b793784652
Allandres <allandres96@gmail.com>
2020-12-08 12:13:11 -0600

    Front - End - Login

--- restauranteASP/Controllers/HomeController.cs
+++ restauranteASP/Controllers/HomeController.cs
@@ -33,5 +33,12 @@ namespace restauranteASP.Controllers
 
             return View();
         }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
     }
 }
